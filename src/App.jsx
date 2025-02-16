47f0b7808a0c72bffe86af48ed1d5e0f0b0248dd
CHUAH, Wei Jie <chuah.weijie@hotmail.com>
2025-02-17 00:07:17 +0800

    feat: adding van details routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,27 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
+import Vans from './pages/Vans';
+
+import './server'
+import VanDetail from './pages/VanDetail';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <header>
+          <Link to='/' className='site-logo'>#VanLife</Link>
           <nav>
-            <Link to='/'>Home</Link>
             <Link to='/about'>About</Link>
+            <Link to="/vans">Vans</Link>
           </nav>
         </header>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
+          <Route path='/vans' element={<Vans/>} />
+          <Route path='/vans/:id' element={<VanDetail/>} />
         </Routes>
       </BrowserRouter>
     </>
